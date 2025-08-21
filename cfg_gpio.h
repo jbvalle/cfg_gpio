12b45d3a0299aa62c9f5d552522251e60e4a21fe
John Bryan Valle <jbvalle.jv@gmail.com>
2025-08-21 02:23:39 +0200

    Added comments

--- cfg_gpio.h
+++ cfg_gpio.h
@@ -2,6 +2,15 @@
 #define CFG_GPIO_H
 #include <stdint.h>
 
+/**
+ * @brief GPIO Mode type.
+ *
+ * Fields:
+ * - GPIO_MODE_INPUT,
+ * - GPIO_MODE_OUTPUT,
+ * - GPIO_MODE_AF,
+ * - GPIO_MODE_ANALOG
+ */
 typedef enum {
     GPIO_MODE_INPUT,
     GPIO_MODE_OUTPUT,
@@ -20,6 +29,17 @@ typedef enum {
     GPIO_PORT_H = 7,
 } GpioPortType;
 
+/**
+ * @brief GPIO configuration type.
+ *
+ * Each element of this struct represents the configuration of a GPIO pin.
+ *
+ * Fields:
+ * - port         : The GPIO port number (e.g., 0, 1, 2…).
+ * - pin          : The pin number within the port.
+ * - mode         : The mode of the GPIO (input, output, etc.).
+ * - defaultLevel : The default output level of the GPIO (high or low).
+ */
 typedef struct{
     uint8_t port;
     uint8_t pin;
@@ -27,6 +47,17 @@ typedef struct{
     GpioLevelType defaultLevel;
 } GpioPinConfigType;
 
+/**
+ * @brief GPIO configuration array.
+ *
+ * Each element of this array represents the configuration of a GPIO pin.
+ *
+ * Fields:
+ * - port         : The GPIO port number (e.g., 0, 1, 2…).
+ * - pin          : The pin number within the port.
+ * - mode         : The mode of the GPIO (input, output, etc.).
+ * - defaultLevel : The default output level of the GPIO (high or low).
+ */
 extern const GpioPinConfigType GpioConfig[];
 
 extern const uint8_t GpioConfigCount;
