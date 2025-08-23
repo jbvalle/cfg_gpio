ef651ba35df2b8c8de5889d94ec338f938f6c767
John Bryan Valle <jbvalle.jv@gmail.com>
2025-08-23 23:02:17 +0200

    first stable cfg_gpio

--- cfg_gpio.c
+++ cfg_gpio.c
@@ -1,4 +1,11 @@
 #include "cfg_gpio.h"
+#include "stm32f401re_regs.h"
+
+GPIO_TypeDef * const GpioPorts[] = {
+    GPIOA,
+    GPIOB,
+    GPIOH
+};
 
 const GpioPinConfigType GpioConfig[] = {
     {.port = GPIO_PORT_A, .pin = 5, .mode = GPIO_MODE_OUTPUT, .defaultLevel = GPIO_LEVEL_LOW},
